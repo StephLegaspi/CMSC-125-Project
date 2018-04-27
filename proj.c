f5f7089931edaa852086431638dff96081bfdb91
StephLegaspi <sylegaspi@up.edu.ph>
2018-04-27 18:34:18 +0800

    Board init finished

--- proj.c
+++ proj.c
@@ -1,9 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+#include<string.h>
 #define BOARD_ROW 30
 #define BOARD_COL 40
+#define STR_LEN 64
+#define MAX_OBJECTS 32
+#define EASY 10
+#define MEDIUM 20
+#define HARD 30
 
-char *readFile(int *noOfObjects){
+int menu(){
+	int choice;
+
+	do{
+		printf("[1] Easy\n");
+		printf("[2] Medium\n");
+		printf("[3] Hard\n");
+		printf("[4] Back to menu\n");
+		printf("Choice: ");
+		scanf("%d", &choice);
+
+		switch(choice){
+			case 1:
+				return EASY;
+				break;
+			case 2:
+				return MEDIUM;
+				break;
+			case 3:
+				return HARD;
+				break;
+		}
+	}while(choice != 4);
+}
+
+void readDescriptions(char desc[MAX_OBJECTS][STR_LEN], int noOfObjects){
+	FILE *fptr;
+	int i, r;
+
+	fptr = fopen("descriptions.txt", "r");
+
+	for(i=0; i<noOfObjects; i++){
+		strcpy(desc[i], "+++");
+	}
+
+	for(i=0; i<noOfObjects; i++){
+		srand(time(NULL));
+		r = rand() % noOfObjects;
+
+		while(strcmp(desc[r], "+++") != 0){
+			r = rand() % noOfObjects;
+		}
+		fgets(desc[r],64,fptr);
+		desc[r][strlen(desc[r])-1] = '\0';
+	}
+}
+
+char *readObjects(int *noOfObjects){
 	FILE *fp;
 	int i;
 	char c, *objs;
@@ -15,24 +69,60 @@ char *readFile(int *noOfObjects){
 
 	for(i=0; i<(*noOfObjects); i++){
 		fscanf(fp,"%c\n", &c);
-		(objs[i]) = c;
+		objs[i] = c;
 	}
 
 	fclose(fp);
 	return objs;
 }
 
+void initBoard(char board[BOARD_ROW][BOARD_COL], char *objects, int boundary){
+	int i, j, x, y;
+
+	for(i=0; i<BOARD_ROW; i++){
+		for(j=0; j<BOARD_COL; j++){
+			board[i][j] = ' ';
+		}
+	}
+
+	for(i=0; i<boundary; i++){
+		srand(time(NULL));
+		x = rand() % 30;
+		y = rand() % 40;
+
+		while(board[x][y] != ' '){
+			x = rand() % 30;
+			y = rand() % 40;
+		}
+
+		board[x][y] = objects[i];
+	}
+}
+
+void printBoard(char board[BOARD_ROW][BOARD_COL]){
+	int i, j;
+
+	for(i=0; i<BOARD_ROW; i++){
+		for(j=0; j<BOARD_COL; j++){
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 int main(){
-	int i, j, noOfObjects;
-	char board[BOARD_ROW][BOARD_COL];
+	int i, j, noOfObjects, boundary;
+	char board[BOARD_ROW][BOARD_COL], desc[MAX_OBJECTS][STR_LEN];
 	char *objects;
 
-	objects = readFile(&noOfObjects);
+	boundary = menu();
+	objects = readObjects(&noOfObjects);
+	initBoard(board, objects, boundary);
+	printBoard(board);
+	readDescriptions(desc, noOfObjects);
 
-	printf("%d\n", noOfObjects);
 	for(i=0; i<noOfObjects; i++){
-		printf("%c", objects[i]);
+		printf("%s\n", desc[i]);
 	}
 
 	return 0;
